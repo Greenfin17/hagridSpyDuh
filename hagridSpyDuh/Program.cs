a554447a5e7a0ee1b76b0bcc8e1ebcd5c32cd27c
thedigitalmenagerie <honeyraeswan@gmail.com>
2021-08-23 19:29:57 -0400

    added gitignore

---
+++ hagridSpyDuh/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hagridSpyDuh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
