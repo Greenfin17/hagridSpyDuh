bc898d1b76a2232d72ae85a51c0266670575d176
Jonathon Joyner <jonathonandon@gmail.com>
2021-08-24 19:44:30 -0500

    Add Changes to repo

--- SpyDuh.API/Repositories/SpyRepo.cs
+++ SpyDuh.API/Repositories/SpyRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SpyDuh.API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,32 @@ namespace SpyDuh.API.Repositories
 {
     public class SpyRepo
     {
+        static List<Spy> _spies = new List<Spy>
+        {
+            new Spy
+            {
+                Name = "James Bond",
+                Id = Guid.NewGuid(),
+                Skills = new List<SpySkills> {SpySkills.Alcoholic, SpySkills.Charisma, SpySkills.Seduction, SpySkills.DefensiveDriving},
+                Services = new List<SpyServices> {SpyServices.SaveTheWorld, SpyServices.IntelligenceGathering},
+                Friends = new List<Guid> {},
+                Enemies = new List<Guid> {},
+                Handlers = new List<Guid> {}
+            },
+             new Spy
+            {
+                Name = "Pierre LaKlutz",
+                Id = Guid.NewGuid(),
+                Skills = new List<SpySkills> {SpySkills.Dancing, SpySkills.Disguises, SpySkills.Fencing,SpySkills.MicrosoftExcel},
+                Services = new List<SpyServices> {SpyServices.Dossier, SpyServices.Theft},
+                Friends = new List<Guid> {},
+                Enemies = new List<Guid> {},
+                Handlers = new List<Guid> {}
+            }
+        };
+        internal IEnumerable<Spy> GetAll()
+        {
+            return _spies;
+        }
     }
 }
