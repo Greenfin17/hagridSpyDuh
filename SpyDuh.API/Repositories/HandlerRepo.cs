357a4e029aba009aca68b006014cbe6755d5d044
John Maple <jmaple@bellsouth.net>
2021-09-22 19:53:07 -0500

    removing hard coded handlers in handler repository.

--- SpyDuh.API/Repositories/HandlerRepo.cs
+++ SpyDuh.API/Repositories/HandlerRepo.cs
@@ -10,27 +10,6 @@ namespace SpyDuh.API.Repositories
 {
     public class HandlerRepo
     {
-        static List<Handler> _handlers = new List<Handler>
-        {
-        new Handler
-            {
-                Name = "M",
-                Id = new Guid("626c99be-a979-4d56-ba8b-3353e4165145"),
-                AgencyName = "M's Agency",
-            },
-             new Handler
-            {
-                Name = "Q",
-                Id = new Guid("a31e693e-1867-4c94-8445-5d4a76aaaf24"),
-                AgencyName = "Q's Agency",
-            },
-             new Handler
-            {
-                Name = "Z",
-                Id = new Guid("3732f2d5-3291-4494-8470-f6e7f719efde"),
-                AgencyName = "Z's Agency",
-            }
-        };
 
         readonly string _connectionString;
         public HandlerRepo(IConfiguration config)
